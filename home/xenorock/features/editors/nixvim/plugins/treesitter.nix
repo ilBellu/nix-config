36074619a47a146c206889d637a3e756b6bd6afd
Marco Bellucci <92112592+ilBellu@users.noreply.github.com>
2024-08-06 01:49:47 +0200

    Minor treesitter options refactor

--- home/xenorock/features/editors/nixvim/plugins/treesitter.nix
+++ home/xenorock/features/editors/nixvim/plugins/treesitter.nix
@@ -1,16 +1,18 @@
 {
   enable = true;
-  indent = true; # Treesitter based indenting
   folding = true; # Treesitter based folding
   nixvimInjections = true; # Enables color highlighting in submodules along with nvim-nix
   # Select treesitter tokens incrementally
-  incrementalSelection = {
-    enable = true;
-    keymaps = {
-      initSelection = "<c-space>";
-      nodeIncremental = "<c-space>";
-      nodeDecremental = "<s-space>";
-      scopeIncremental = "<c-s>";
+  settings = {
+    indent.enable = true; # Treesitter based indenting
+    incrementalSelection = {
+      enable = true;
+      keymaps = {
+        init_selection = "<c-space>";
+        node_incremental = "<c-space>";
+        node_decremental = "<s-space>";
+        scope_incremental = "<c-s>";
+      };
     };
   };
 }
